9b1b7616193f5020c2e31f154bd0baa14d668473
Chris Meffley <cmeffley@gmail.com>
2021-02-01 23:11:00 -0500

    got card function to work

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 //console.log('Connected');
-
 const hat = [
   {
-    name: 'Gryffindor',
+    name: 'Chris',
+    house: 'Gryffindor',
     values: ['bravery ', 'daring ', 'nerve ', 'chivarly'],
     animal: 'Lion',
     colors: 'Scarlet and Gold',
   },
   {
-    name: 'Hufflepuff',
+    name: 'Jackson',
+    house: 'Hufflepuff',
     values: ['hard work ', 'dedication ', 'patience ', 'loyalty ', 'fair play'],
     animal: 'Badger',
     colors: 'Yellow and Black',
   },
   {
-    name: 'Ravenclaw',
+    name: 'Eliza',
+    house: 'Ravenclaw',
     values: ['intelligence ', 'knowledge ', 'curiosity ', 'creativity ', 'wit'],
     animal: 'Eagle',
     colors: 'Blue and Bronze',
   },
   {
-    name: 'Slytherin',
+    name: 'Kacie',
+    house: 'Slytherin',
     values: ['ambition ', 'leadership ', 'self-preservation ', 'cunning ', 'resourcefulness'],
     animal: 'Serpent',
     colors: 'Emerald Green and Silver', 
@@ -32,34 +35,48 @@ const printToDom = (divId, textToPrint) => {
   selectDiv.innerHTML = textToPrint;
 };
 
-const createForm = (domString) => {
-    `<form>
+const createForm = () => {
+  let form = `<form>
     <div class="mb-3">
     <label for="student" class="form-label">Student</label>
     <input type="text" class="form-control" id="student" required>
     </div>
-    <button type="submit" class="btn btn-primary" id="sort1">Sort!</button>
+    <button type="submit" class="btn btn-info" id="sort1">Sort!</button>
     </form>`
+    printToDom('#form', form);
+    document.querySelector('#sort1').addEventListener('click', cardCreated);
+    
   };
-  printToDom(domString);
 
   
   
-  function cardCreated(hatArr) {
-  let domString = '';
+  function cardCreated() {
+    let domString = '';
+    hat.forEach((element, i) => {
+      domString += `<div class="card mb-3" style="width: 18rem;" id=${i}>      
+        <div class="card-body">
+          <p class="card-text">${element.name}</p>
+          <p class="card-text">${element.values}</p>
+          <p class="card-text">${element.animal}</p>
+          <p class="card-text">${element.colors}</p>
+          <button type="button" class="btn btn-danger" id="${i}">Expel!</button>
+        </div>
+        </div>`
+    });
+    printToDom('#hats', domString);
+    
+  }
 
-  hatArr.forEach((element, i) => {
-    domString += `< class="card mb-3" style="width: 18rem;" id=${i}>      
-      <div class="card-body">
-        <p class="card-text">${element.name}</p>
-        <p class="card-text">${element.values}</p>
-        <p class="card-text">${element.animal}</p>
-        <p class="card-text">${element.colors}</p>
-        <button type="button" class="btn btn-danger" id="${i}">Expel!</button>
-      </div>`;
-  });
-  printToDom('#hats', domString);
-}
+
+const formInfo = (e) => {
+  e.preventDefault();  
+
+  const name = document.querySelector('#student').value;
+
+  const newHouse = Math.floor(Math.random() * hats.length);
+  const house = hats[newhats];
+
+
 
 
 
@@ -67,15 +84,13 @@ const createForm = (domString) => {
 
 
 const useButton = (e) => {
-  const buttonId = e.target.id;
 
-  document.querySelector('#start-sort').addEventListener('click', createForm);
-  document.querySelector('#start1').addEventListener('click', cardCreated);
+  document.querySelector('#start-sort').addEventListener('click', createForm);  
+  
 };
 
 const init = () => {
-createForm();
-cardCreated(hat);
+  useButton();
 };
 
 init();
